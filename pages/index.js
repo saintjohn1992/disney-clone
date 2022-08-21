8d6ca62f413cb235b85d69fddcf457ca45dc2e22
saintjohn1992 <saintjohnshane@outlook.com>
2022-08-21 16:22:22 +0200

    setting up project framework and installing dependencies

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { gql, GraphQLClient } from 'graphql-request'
+
+export const getStaticProps = async () => {
+  
+}
+
+export default function Home() {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
